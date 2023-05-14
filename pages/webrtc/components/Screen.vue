73425ae34f6fc3fc93b8d614c9ac53f6e0c9d49b
seemrcola <seemrcola@163.com>
2023-05-14 23:29:06 +0800

    feat:[webrtc-study] 基本api使用

--- pages/webrtc/components/Screen.vue
+++ pages/webrtc/components/Screen.vue
@@ -1,4 +1,7 @@
 <script setup lang='ts'>
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css'
+
 const replayVideo = ref<HTMLVideoElement | null>(null)
 
 const constraints = {
@@ -14,7 +17,29 @@ async function catchScreen() {
   const stream = await navigator.mediaDevices.getDisplayMedia(constraints)
   // 赋值给video标签
   replayVideo.value!.srcObject = stream
+  replayVideo.value!.play()
 }
+
+const code = `
+// code base on vue3
+const replayVideo = ref<HTMLVideoElement | null>(null)
+
+const constraints = {
+  video: true,
+  audio: false,
+}
+
+async function catchScreen() {
+  // 捕获屏幕，进行录制
+  if (!navigator.mediaDevices.getDisplayMedia)
+    return alert('浏览器不支持屏幕捕获')
+  // 获取屏幕流 使用的是getDisplayMedia
+  const stream = await navigator.mediaDevices.getDisplayMedia(constraints)
+  // 赋值给video标签
+  replayVideo.value!.srcObject = stream
+  replayVideo.value!.play()
+}`
+const codeHtml = Prism.highlight(code, Prism.languages.javascript, 'javascript')
 </script>
 
 <template>
@@ -28,5 +53,12 @@ async function catchScreen() {
     <div h="300px" p-2 b="1px dashed gray">
       <video ref="replayVideo" w-full />
     </div>
+
+    <pre
+      mt-4 px-2 max-h="450px" overflow-auto
+      b="1px dashed gray"
+    >
+      <code v-html="codeHtml" />
+    </pre>
   </div>
 </template>
