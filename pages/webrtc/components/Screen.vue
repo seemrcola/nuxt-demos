f7cfcfcf35eb8a494fe59e846faf3e1c571d5963
seemrcola <seemrcola@163.com>
2023-05-15 11:59:47 +0800

    feat: [webrtc study] 增加停止屏幕抓取的功能

--- pages/webrtc/components/Screen.vue
+++ pages/webrtc/components/Screen.vue
@@ -3,6 +3,7 @@ import Prism from 'prismjs'
 import 'prismjs/themes/prism.css'
 
 const replayVideo = ref<HTMLVideoElement | null>(null)
+const screenShareStream = ref<MediaStream>()
 
 const constraints = {
   video: true,
@@ -15,14 +16,26 @@ async function catchScreen() {
     return alert('浏览器不支持屏幕捕获')
   // 获取屏幕流 使用的是getDisplayMedia
   const stream = await navigator.mediaDevices.getDisplayMedia(constraints)
+
+  screenShareStream.value = stream
+
   // 赋值给video标签
   replayVideo.value!.srcObject = stream
   replayVideo.value!.play()
 }
 
+async function stop() {
+  // 停止屏幕共享
+  if (screenShareStream.value) {
+    const [screenTrack] = screenShareStream.value.getTracks()
+    screenTrack.stop()
+    screenShareStream.value = undefined
+  }
+}
 const code = `
 // code base on vue3
 const replayVideo = ref<HTMLVideoElement | null>(null)
+const screenShareStream = ref<MediaStream>()
 
 const constraints = {
   video: true,
@@ -35,10 +48,21 @@ async function catchScreen() {
     return alert('浏览器不支持屏幕捕获')
   // 获取屏幕流 使用的是getDisplayMedia
   const stream = await navigator.mediaDevices.getDisplayMedia(constraints)
+  screenShareStream.value = stream
   // 赋值给video标签
   replayVideo.value!.srcObject = stream
   replayVideo.value!.play()
-}`
+}
+
+async function stop() {
+  // 停止屏幕共享
+  if (screenShareStream.value) {
+    const [screenTrack] = screenShareStream.value.getTracks()
+    screenTrack.stop()
+    screenShareStream.value = undefined
+  }
+}
+`
 const codeHtml = Prism.highlight(code, Prism.languages.javascript, 'javascript')
 </script>
 
@@ -48,13 +72,20 @@ const codeHtml = Prism.highlight(code, Prism.languages.javascript, 'javascript')
       <div btn mr-4 @click="catchScreen">
         Catch Screen
       </div>
+      <div btn mr-4 @click="stop">
+        Stop Catch Screen
+      </div>
     </div>
 
     <div p-2 b="1px dashed gray">
       <video ref="replayVideo" w-full />
     </div>
 
-    <div b="1px dashed gray" overflow-auto mt-4>
+    <div
+      b="1px dashed gray"
+      max-h="[calc(100vh-400px)]"
+      overflow-auto mt-4
+    >
       <NCode :code-html="codeHtml" />
     </div>
   </div>
