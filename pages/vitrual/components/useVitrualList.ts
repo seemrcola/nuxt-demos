8c33930b234bf738e3528b3ac76bc7b311c07fb6
seemrcola <seemrcola@163.com>
2023-05-22 14:24:12 +0800

    feat: [vitrual list] 代码整理

--- pages/vitrual/components/useVitrualList.ts
+++ pages/vitrual/components/useVitrualList.ts
@@ -8,13 +8,9 @@ export function useVitrualList(list: any[], selector: string, options: Options) 
   // 滚动相关变量>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   const { itemHeight, containerHeight } = options
   let vitrualOffset = 0 // 滚动高度
-  // 缓存长度=======================
-  // todo-------------------------
-  // =============================
   const renderOffset = ref(0) // 渲染偏移量
   const renderList = ref<any[]>([]) // 渲染列表
-  // 渲染数量
-  const renderCount = Math.ceil(containerHeight / itemHeight)
+  const RENDER_COUNT = Math.ceil(containerHeight / itemHeight) // 渲染数量
 
   // 滚动条相关变量>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   const { scrollbar = false } = options // 是否显示滚动条
@@ -22,6 +18,7 @@ export function useVitrualList(list: any[], selector: string, options: Options) 
   let startY = 0 // 鼠标按下时的位置
   let deltaY = 0 // 鼠标移动的距离
   let barHeight = 0 // 滚动条高度
+  let ratio = 0 // 滚动条高度与容器高度的比值
   const scrollbarRef = ref<HTMLElement | null>(null) // 滚动条ref
 
   // =====================================滚动相关================================================================
@@ -63,8 +60,8 @@ export function useVitrualList(list: any[], selector: string, options: Options) 
       return true
     }
     // 当滚动到底部时，不再向下滚动
-    if (deltaY > 0 && vitrualOffset >= (list.length - renderCount) * itemHeight) {
-      vitrualOffset = (list.length - renderCount) * itemHeight
+    if (deltaY > 0 && vitrualOffset >= (list.length - RENDER_COUNT) * itemHeight) {
+      vitrualOffset = (list.length - RENDER_COUNT) * itemHeight
       return true
     }
     return false
@@ -75,7 +72,7 @@ export function useVitrualList(list: any[], selector: string, options: Options) 
     // 我们二分查找返回的就是第一个大于 vitrualOffset 的值的下标
     // 需要注意的是我们执行 mid * itemHeight 的时候，mid是8，则表示前8个元素的高度总和。
     const start = bs(list.length) // start * itemHeight 必定大大于 vitrualOffset，且start不代表开始下标，而是前start个元素的高度总和
-    const end = start + renderCount // 计算end
+    const end = start + RENDER_COUNT // 计算end
     // ========================================================
     // 计算偏移量================================================
     const heightSum = start * itemHeight
@@ -90,37 +87,6 @@ export function useVitrualList(list: any[], selector: string, options: Options) 
     return [start - 1, end + 1]
   }
 
-  // throttle函数实现
-  function throttle(this: any, fn: Function, delay: number) {
-    let timer: any = null
-    return (...args: any[]) => {
-      if (!timer) {
-        timer = setTimeout(() => {
-          fn.apply(this, args)
-          timer = null
-        }, delay)
-      }
-    }
-  }
-
-  // 写一个二分查找算法
-  // 找到第一个大于等于 vitrualOffset 的值的下标
-  function bs(len: number) {
-    let left = -1
-    let right = len + 1
-    while (left < right - 1) {
-      const mid = (left + right) >> 1
-      const offset = mid * itemHeight
-      if (offset > vitrualOffset)
-        right = mid
-      else if (offset < vitrualOffset)
-        left = mid
-      else
-        return mid + 1
-    }
-    return left + 1
-  }
-
   function render(items: any[]) {
     renderList.value = items
   }
@@ -131,15 +97,9 @@ export function useVitrualList(list: any[], selector: string, options: Options) 
     }
   },
   )
-
-  function init() {
-    nextTick(() => {
-      initContainer()
-    })
-  }
   // ======================================================滚动相关end======================================================
 
-  // =======================================================模拟滚动条=========================================================
+  // ======================================================模拟滚动条========================================================
 
   function generateScrollbar() {
     const scrollbar = document.createElement('div')
@@ -232,12 +192,10 @@ export function useVitrualList(list: any[], selector: string, options: Options) 
     const top = parseInt(scrollbarRef.value!.style.top)
     const cssTop = top + deltaY // 滚动条的css top值
     const finalCssTop = cssOverflow(cssTop) // 滚动条的css top值经过处理后的值
-
     scrollbarRef.value!.style.top = `${finalCssTop}px`
 
     // 滚动条移动的距离，转换成列表移动的距离，需要乘以一个比例
     transformCssTopToVitrualOffset(finalCssTop)
-
     // !!根据偏移量，计算出列表的下标，然后渲染列表=========
     const [start, end] = calcBlocks(vitrualOffset)
     render(list.slice(start, end))
@@ -260,28 +218,65 @@ export function useVitrualList(list: any[], selector: string, options: Options) 
     return cssTop
   }
 
-  scrollbar && initScrollbar()
   // ========================================================================================================================
 
-  // ===========================================转换工具===========================================
+  // ===========================================工具===========================================
+  // throttle函数实现
+  function throttle(this: any, fn: Function, delay: number) {
+    let timer: any = null
+    return (...args: any[]) => {
+      if (!timer) {
+        timer = setTimeout(() => {
+          fn.apply(this, args)
+          timer = null
+        }, delay)
+      }
+    }
+  }
+
+  // 找到第一个大于等于 vitrualOffset 的值的下标
+  function bs(len: number) {
+    let left = -1
+    let right = len + 1
+    while (left < right - 1) {
+      const mid = (left + right) >> 1
+      const offset = mid * itemHeight
+      if (offset > vitrualOffset)
+        right = mid
+      else if (offset < vitrualOffset)
+        left = mid
+      else
+        return mid + 1
+    }
+    return left + 1
+  }
+
   function transformCssTopToVitrualOffset(cssTop: number) {
     // 滚动条的css top值，转换成列表的偏移量
-    const listHeight = list.length * itemHeight - containerHeight
-    const scrollHeight = containerHeight - barHeight
-    const ratio = listHeight / scrollHeight
     vitrualOffset = cssTop * ratio
     renderOffset.value = vitrualOffset
   }
 
   function transformVitrualToCssTop(vitrualOffset: number) {
     // 列表的偏移量，转换成滚动条的css top值
-    const listHeight = list.length * itemHeight - containerHeight
-    const scrollHeight = containerHeight - barHeight
-    const ratio = listHeight / scrollHeight
     const cssTop = vitrualOffset / ratio
     return cssTop
   }
   // ==================================================================================
+
+  function init() {
+    nextTick(() => {
+      initContainer()
+      if (scrollbar) {
+        // 初始化滚动条
+        initScrollbar()
+        // 计算滚动条和列表的比例
+        const listHeight = list.length * itemHeight - containerHeight
+        const scrollHeight = containerHeight - barHeight
+        ratio = listHeight / scrollHeight
+      }
+    })
+  }
 
   return {
     init,
