b32f3978d4bb04ecc521688f2006bd669a75b65c
seemrcola <seemrcola@163.com>
2023-05-30 22:12:18 +0800

    feat: [editor] 缩放基础

--- components/Shape/Bubble.vue
+++ components/Shape/Bubble.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-  <div i-eos-icons:bubble-loading wh-full color="orange" />
+  <div i-eos-icons:bubble-loading h="100px" w="100px" color="orange" />
 </template>
--- components/Toys/Lifegame.vue
+++ components/Toys/Lifegame.vue
@@ -129,7 +129,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div b wh-full>
+  <div b>
     <canvas ref="canvas" height="400" width="400" />
   </div>
 </template>
